f88ebcdf3927a85935e880ea3f8c2f5da204797a
Manuel Ramirez Coronel <ra_cm@outlook.com>
2022-04-04 16:52:08 -0500

    Add a QR Code by Permalink

--- custom/functions.php
+++ custom/functions.php
@@ -7,11 +7,38 @@
  * @since    1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 /**
  * functions.php
  * Add PHP snippets here
  */
+/**
+ * Add a custom product data tab
+ */
+
+add_filter('woocommerce_product_tabs', 'qr_code_for_woocommerce_new_product_tab');
+
+function qr_code_for_woocommerce_new_product_tab($tabs)
+{
+    // Adds the new tab
+    $tabs['qr_code_for_woocommerce'] = array(
+        'title' => __('QR Code', 'woocommerce'),
+        'priority' => 50,
+        'callback' => 'qr_code_for_woocommerce_product_tab_content',
+    );
+    return $tabs;
+}
+
+function qr_code_for_woocommerce_product_tab_content()
+{
+	$product_url = get_permalink( $product_id );
+    ?>
+	<h2>
+		QR Code
+	</h2>
+	<?php
+	echo "<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={$product_url}&choe=UTF-8' title='Link to Google.com' />";    
+}
--- qr-code-for-woocommerce.php
+++ qr-code-for-woocommerce.php
@@ -11,53 +11,58 @@
  * @package QR_Code_for_WooCommerce
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 /**
  * Main QR_Code_for_WooCommerce Class
  *
  * @class QR_Code_for_WooCommerce
- * @version	1.0.0
+ * @version    1.0.0
  * @since 1.0.0
- * @package	QR_Code_for_WooCommerce
+ * @package    QR_Code_for_WooCommerce
  */
-final class QR_Code_for_WooCommerce {
+final class QR_Code_for_WooCommerce
+{
 
-	/**
-	 * Set up the plugin
-	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'qr_code_for_woocommerce_setup' ), -1 );
-		require_once( 'custom/functions.php' );
-	}
+    /**
+     * Set up the plugin
+     */
+    public function __construct()
+    {
+        add_action('init', array($this, 'qr_code_for_woocommerce_setup'), -1);
+        require_once 'custom/functions.php';
+    }
 
-	/**
-	 * Setup all the things
-	 */
-	public function qr_code_for_woocommerce_setup() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'qr_code_for_woocommerce_css' ), 999 );
-		add_action( 'wp_enqueue_scripts', array( $this, 'qr_code_for_woocommerce_js' ) );
-	}
+    /**
+     * Setup all the things
+     */
+    public function qr_code_for_woocommerce_setup()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'qr_code_for_woocommerce_css'), 999);
+        add_action('wp_enqueue_scripts', array($this, 'qr_code_for_woocommerce_js'));
+    }
 
-	/**
-	 * Enqueue the CSS
-	 *
-	 * @return void
-	 */
-	public function qr_code_for_woocommerce_css() {
-		wp_enqueue_style( 'custom-css', plugins_url( '/custom/style.css', __FILE__ ) );
-	}
+    /**
+     * Enqueue the CSS
+     *
+     * @return void
+     */
+    public function qr_code_for_woocommerce_css()
+    {
+        wp_enqueue_style('custom-css', plugins_url('/custom/style.css', __FILE__));
+    }
 
-	/**
-	 * Enqueue the Javascript
-	 *
-	 * @return void
-	 */
-	public function qr_code_for_woocommerce_js() {
-		wp_enqueue_script( 'custom-js', plugins_url( '/custom/custom.js', __FILE__ ), array( 'jquery' ) );
-	}
+    /**
+     * Enqueue the Javascript
+     *
+     * @return void
+     */
+    public function qr_code_for_woocommerce_js()
+    {
+        wp_enqueue_script('custom-js', plugins_url('/custom/custom.js', __FILE__), array('jquery'));
+    }
 } // End Class
 
 /**
@@ -65,11 +70,12 @@ final class QR_Code_for_WooCommerce {
  *
  * @return void
  */
-function qr_code_for_woocommerce_main() {
-	new QR_Code_for_WooCommerce();
+function qr_code_for_woocommerce_main()
+{
+    new QR_Code_for_WooCommerce();
 }
 
 /**
  * Initialise the plugin
  */
-add_action( 'plugins_loaded', 'qr_code_for_woocommerce_main' );
+add_action('plugins_loaded', 'qr_code_for_woocommerce_main');
